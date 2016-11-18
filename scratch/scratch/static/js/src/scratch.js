0e5a0dc9d61483e016edc81bab8a13deb37e832b
Ruslan Simonenko <ruslan.simonenko@gmail.com>
2016-11-19 01:44:57 +0200

    Basic XBlock plugin with Blockly integrated.

---
+++ scratch/scratch/static/js/src/scratch.js
@@ -0,0 +1,25 @@
+/* Javascript for ScratchXBlock. */
+function ScratchXBlock(runtime, element) {
+
+    function updateCount(result) {
+        $('.count', element).text(result.count);
+    }
+
+    var handlerUrl = runtime.handlerUrl(element, 'increment_count');
+
+    $('p', element).click(function (eventObject) {
+        $.ajax({
+            type: "POST",
+            url: handlerUrl,
+            data: JSON.stringify({"hello": "world"}),
+            success: updateCount
+        });
+    });
+
+    $(function ($) {
+        console.log(Blockly);
+
+        var workspace = Blockly.inject('blocklyDiv',
+            {toolbox: document.getElementById('toolbox')});
+    });
+}
